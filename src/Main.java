4204013c93e1c03b9e7a1946f5c8212fc32b114b
MariiaMerkel <mariia_merkel@mail.ru>
2024-01-11 08:35:43 +0300

    начала делать доп задания

--- src/Main.java
+++ src/Main.java
@@ -1,56 +1,37 @@
 public class Main {
     public static void main(String[] args) {
-        isLeapYear(2000);
-        recommendOS(2, 2000);
-        printDelivery(calculateDeliveryDays(95));
+        System.out.println(min(9, 5));
+        System.out.println(isEven(4));
+        System.out.println(raiseSecondPower(4));
+        System.out.println(raiseThirdPower(4));
+        checkNumber(16);
+
     }
 
-    public static void isLeapYear(int year) {
-        if (year >= 1584 && year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
-            System.out.println(year + " год — високосный год");
-        } else {
-            System.out.println(year + " год — невисокосный год");
-        }
+    public static int min(int first, int second) {
+        return first < second ? first : second;
     }
 
-    public static void recommendOS(int clientOS, int clientDeviceYear) {
-        if (clientOS == 0) {
-            if (clientDeviceYear >= 2015) {
-                System.out.println("Установите обычную версию приложения для Android по ссылке");
-            } else {
-                System.out.println("Установите облегченную версию приложения для Android по ссылке");
-            }
-        } else if (clientOS == 1) {
-            if (clientDeviceYear >= 2015) {
-                System.out.println("Установите обычную версию приложения для iOS по ссылке");
-            } else {
-                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-            }
-        } else {
-            System.out.println("Такой ОС не существует");
-        }
+    public static boolean isEven(int number) {
+        return number % 2 == 0;
     }
 
-    public static int calculateDeliveryDays(int distance) {
-        int days = 1;
-        if (distance > 20) {
-            days++;
-        }
-        if (distance > 60) {
-            days++;
-        }
-        if (distance > 100) {
-            return 0;
-        }
-        return days;
+    public static int raiseSecondPower(int number) {
+        return number * number;
     }
 
-    private static void printDelivery(int days) {
-        String wordForm = days > 1 ? " дня" : " день";
-        if (days != 0) {
-            System.out.println("Потребуется " + days + wordForm);
-        } else {
-            System.out.println("Доставки нет");
+    public static int raiseThirdPower(int number) {
+        return number * number * number;
+    }
+
+    public static void checkNumber(int number) {
+        System.out.println(number >= 10);
+    }
+
+    public static int[] sortAscending(int[] numbers) {
+        int[] result = new int[numbers.length];
+        for (int i = 0; i < result.length; i++) {
+
         }
 
     }
